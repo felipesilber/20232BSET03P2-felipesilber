94e3aac92b9ba4705ef18a281597c9803e97933e
felipesilber <felipe.silberberg@sou.inteli.edu.br>
2023-12-15 14:43:20 -0300

    mudança no codigo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+
 
 const app = express();
 const port = 3000;
@@ -11,29 +13,85 @@ app.use(bodyParser.json());
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, votes INT)");
 });
 
-app.post('/cats', (req, res) => {
+app.post('/cats', [
+  body('name').isString().escape(),
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   const name = req.body.name;
-  db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
+  const instruction = db.prepare("INSERT INTO cats (name, votes) VALUES (?, 0)");
+
+  instruction.run(name, function(err) {
+    instruction.finalize();
+
     if (err) {
-      res.status(500).send("Erro ao inserir no banco de dados");
+      res.status(500).json({ error: "Erro ao inserir no banco de dados" });
     } else {
       res.status(201).json({ id: this.lastID, name, votes: 0 });
     }
   });
 });
 
-app.post('/dogs', (req, res) => {
-  
+app.post('/dogs', [
+  body('name').isString().escape(),
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const name = req.body.name;
+  const instruction = db.prepare("INSERT INTO dogs (name, votes) VALUES (?, 0)");
+
+  instruction.run(name, function(err) {
+    instruction.finalize();
+
+    if (err) {
+      res.status(500).json({ error: "Erro ao inserir no banco de dados" });
+    } else {
+      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
 });
 
 app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+  const animalType = req.params.animalType;
+  const id = req.params.id;
+
+  const check = db.prepare(`SELECT COUNT(*) as count FROM ${animalType} WHERE id = ?`);
+  check.get(id, (err, row) => {
+    check.finalize();
+
+    if (err) {
+      res.status(500).json({ error: "Erro ao verificar o banco de dados" });
+    } else {
+      const count = row.count;
+
+      if (count === 0) {
+        res.status(404).json({ error: "Registro não encontrado" });
+      } else {
+        const update = db.prepare(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ?`);
+        update.run(id, function (err) {
+          update.finalize();
+
+          if (err) {
+            res.status(500).json({ error: "Erro ao atualizar o banco de dados" });
+          } else {
+            res.status(200).send("Voto computado");
+          }
+        });
+      }
+    }
+  });
 });
 
 app.get('/cats', (req, res) => {
@@ -47,7 +105,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao consultar o banco de dados");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
